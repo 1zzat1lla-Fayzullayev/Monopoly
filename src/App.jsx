769731c230537d68282a0210a467f7bb40ab3ab0
Forestest <hosting0web258@gmail.com>
2024-07-22 09:14:09 +0500

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Login from "./pages/login/login";
+import Register from "./pages/register/register";
 
 function App() {
   return (
-    <>
-      <h2>Monopoly project</h2>
-    </>
-  )
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
