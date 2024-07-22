5db55aa1155a006dcabe375a3109194321519b5f
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-07-22 10:18:16 +0500

    add nav

--- src/components/loginNavbar.jsx
+++ src/components/loginNavbar.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import Wrapper from '../layout/Wrapper'
 
 function LoginNavbar() {
-  return (
-	<>
-		aa
-	</>
-  )
+	return (
+		<>
+			<nav>
+				<Wrapper>
+					<div className='flex items-center justify-between p-[18px]'>
+						<img src='/assets/лого.png' alt='logo' />
+						<div className='flex items-center gap-[20px]'>
+							<button className='px-[26px] py-[16px] bg-[#E9ECFF] rounded-[40px] text-[17px]'>
+								Правила игры
+							</button>
+							<button className='px-[26px] py-[16px] bg-[#E9ECFF] rounded-[40px] text-[17px]'>
+								О проекте
+							</button>
+						</div>
+					</div>
+				</Wrapper>
+			</nav>
+		</>
+	)
 }
 
 export default LoginNavbar
