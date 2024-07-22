68810ef5f4265658dadbe30df90373c3d6c91592
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-07-22 09:23:43 +0500

    add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
-import Login from "./pages/login/login";
-import Register from "./pages/register/register";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Login from './pages/login/login'
+import Register from './pages/register/register'
+import LoginNavbar from './components/loginNavbar'
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Routes>
-          <Route exact path="/" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-        </Routes>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<LoginNavbar />
+			<div>
+				<Routes>
+					<Route exact path='/' element={<Login />}></Route>
+					<Route path='/register' element={<Register />}></Route>
+				</Routes>
+			</div>
+		</Router>
+	)
 }
 
-export default App;
+export default App
